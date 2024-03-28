953ec95be112d0c799c32660887d40373ebd09d0
ghostNoté <selcukbilgen@hotmail.com>
2024-03-29 00:22:01 +0300

    parameters, event callback sample.

--- BooksStore/Program.cs
+++ BooksStore/Program.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BooksStore;
 using BooksStore.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,4 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
+builder.Services.AddScoped<IBooksService, LocalBooksService>();
 await builder.Build().RunAsync();
