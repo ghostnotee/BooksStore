4215460d90d82bb20ecffd29a61500d6c68f7651
ghostNoté <selcukbilgen@hotmail.com>
2024-04-01 21:53:09 +0300

    get by id added.

--- BooksStore/Services/LocalBooksService.cs
+++ BooksStore/Services/LocalBooksService.cs
@@ -8,13 +8,17 @@ public class LocalBooksService : IBooksService
     [
         new Book
         {
+            Id = Guid.NewGuid().ToString(),
             AuthorName = "John Smith",
             PublishingDate = new DateTime(2021, 01, 12),
-            Title = "Blazor WebAssembly Guide"
+            Title = "Blazor WebAssembly Guide",
+            Description = "Test",
+            Price = 10000
         },
 
         new Book
         {
+            Id = Guid.NewGuid().ToString(),
             AuthorName = "John Smith",
             PublishingDate = new DateTime(2022, 03, 13),
             Title = "Mastering Blazor WebAssembly"
@@ -22,6 +26,7 @@ public class LocalBooksService : IBooksService
 
         new Book
         {
+            Id = Guid.NewGuid().ToString(),
             AuthorName = "John Smith",
             PublishingDate = new DateTime(2022, 08, 01),
             Title = "Learning Blazor from A to Z"
@@ -31,5 +36,11 @@ public class LocalBooksService : IBooksService
     public Task<List<Book>> GetAllBooksAsync()
     {
         return Task.FromResult(_allBooks);
+    }
+
+    public Task<Book?> GetBookByIdAsync(string? id)
+    {
+        var book = _allBooks.SingleOrDefault(b => b.Id == id);
+        return Task.FromResult(book);
     }
 }
